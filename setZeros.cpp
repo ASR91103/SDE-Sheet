a55298d099c8b1e8f0ac9c044db0adc7110918a0
Shradha Arsewar <146162679+ASR91103@users.noreply.github.com>
2025-02-21 13:03:37 +0530

    Create setZeros.cpp

---
+++ setZeros.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> setZeroes(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<int> row(n,0);
+    vector<int> col(m,0);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    vector<vector<int>> ans = setZeroes(matrix);
+
+    cout << "The Final matrix is: "<<endl;
+    for (auto it : ans) {
+        for (auto elements : it) {
+            cout << elements << " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
